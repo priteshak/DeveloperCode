bf128dd6364e85e1516faf9dae89287dc456c06f
unknown <4444pritesh@gmail.com>
2024-10-05 16:13:40 +0530

    First Developer Push

---
+++ src/main/java/DeveloperBuild/DeveloperBuildPush/App.java
@@ -0,0 +1,13 @@
+package DeveloperBuild.DeveloperBuildPush;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
